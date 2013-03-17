7a3b0ca42ec1b03ede93a48db552b3692aa6a08d
Carl-Erik Kopseng <carlerik@gmail.com>
2013-03-17 21:09:37 +0100

    Formatted description.

--- TicTacToeTDD/src/TicTacToeTest.java
+++ TicTacToeTDD/src/TicTacToeTest.java
@@ -4,30 +4,37 @@ import static org.junit.Assert.assertTrue;
 
 /**
  * TDD as if you Meant It
+ * <h2>The Rules</h2>
+ * <ol>
+ * <li>Write <em>exactly one</em> new test, the smallest test you can that seems to point in the direction of a solution</li>
+ * <li>See it fail</li>
+ * <li>Make the test from (1) pass by writing the least implementation code you can <em>in the test method. </em></li>
+ * <li>Refactor to remove duplication, and otherwise as required to improve the design. Be strict about using these moves:
+ * <ol>
+ * <li><strong>you want a new method—wait until refactoring time, then&#8230; </strong>create new (non-test) methods by doing one of these, and in no other way:</li>
+ * <ol>
+ * <li>preferred: do Extract Method on implementation code created as per (3) to create a new method in the test class, or</li>
+ * <li>if you must: move implementation code as per (3) into an existing implementation method</li>
+ * </ol>
+ * <li><strong>you want a new class—<strong>wait until refactoring time, then&#8230; </strong></strong>create non-test classes to provide a destination for a Move Method and for no other reason</li>
+ * <ol>
+ * <li>populate implementation classes with methods by doing Move Method, and no other way</li>
+ * </ol>
+ * </ol>
+ * </li>
+ * </ol>
  * <p/>
- * The Rules *
- * 1. Write exactly one new test, the smallest test you can that seems to point in the direction of a solution
- * 2. See it fail
- * 3. Make the test from (1) pass by writing the least implementation code you can in the test method.
- * 4. Refactor to remove duplication, and otherwise as required to improve the design. Be strict about using these moves:
- * 1. you want a new method—wait until refactoring time, then… create new (non-test) methods by doing one of these, and in no other way:
- * 1. preferred: do Extract Method on implementation code created as per (3) to create a new method in the test class, or
- * 2. if you must: move implementation code as per (3) into an existing implementation method
- * 2. you want a new class—wait until refactoring time, then… create non-test classes to provide a destination for a Move Method and for no other reason
- * 1. populate implementation classes with methods by doing Move Method, and no other way
+ * <p>The member of the pair without their hands on the keyboard must be very strict in enforcing these rules, especially 4.1 and 4.2</p>
  * <p/>
- * The member of the pair without their hands on the keyboard must be very strict in enforcing these rules, especially 4.1 and 4.2
+ * <p>After some respectable time coding, contrast and compare solutions. Consider the classes created. How many? How big? What mutable state? Consider the methods created How many? How long? Apply a few simple design metrics. How was the experience of working this way different from the usual? How could these ideas be applied in your day job?</p>
  * <p/>
- * After some respectable time coding, contrast and compare solutions. Consider the classes created. How many? How big?
- * What mutable state? Consider the methods created How many? How long? Apply a few simple design metrics.
- * How was the experience of working this way different from the usual? How could these ideas be applied in your day job?
- * <p/>
- * <p/>
- * a game is over when all fields are taken
- * a game is over when all fields in a column are taken by a player
- * a game is over when all fields in a row are taken by a player
- * a game is over when all fields in a diagonal are taken by a player
- * a player can take a field if not already taken
+ * <ul>
+ * <li>a game is over when all fields are taken</li>
+ * <li>a game is over when all fields in a column are taken by a player</li>
+ * <li>a game is over when all fields in a row are taken by a player        </li>
+ * <li>a game is over when all fields in a diagonal are taken by a player        </li>
+ * <li>a player can take a field if not already taken                                 </li>
+ * </ul>
  * players take turns taking fields until the game is over
  *
  * @see <a href="http://cumulative-hypotheses.org/2011/08/30/tdd-as-if-you-meant-it">original article</a>
@@ -35,13 +42,13 @@ import static org.junit.Assert.assertTrue;
  * @see <a href="http://www.codersdojo.com/review/f80623b0fc6c2951dbcd8dc33f9dfb842212e788?page=19#first_modification">Stepwise TDD as if you meant it</a>
  */
 public class TicTacToeTest {
-    @Test
-    public void should_end_game_when_all_fields_are_taken() {
-        assertTrue(isFinished());
-    }
+	@Test
+	public void should_end_game_when_all_fields_are_taken() {
+		assertTrue(isFinished());
+	}
 
-    private boolean isFinished() {
-        return true;
-    }
+	private boolean isFinished() {
+		return true;
+	}
 
 }
