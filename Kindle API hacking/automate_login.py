bb0b80e4859194004139341a26f514854ccb4c4d
Carl-Erik Kopseng <carlerik@gmail.com>
2013-06-15 12:24:12 +0200

    Reads username and password from the command line

--- Kindle API hacking/automate_login.py
+++ Kindle API hacking/automate_login.py
@@ -1,7 +1,6 @@
 import requests, re, sys
 from bs4 import BeautifulSoup
 
-my_email = 'carlerik@gmail.com'
 user_agent = "User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36" 
 extra_headers = { 'User-Agent' : user_agent }
 
@@ -16,5 +15,5 @@ for a in attributes:
     form_values[a.get('name')] = a.get('value')
 form_values.pop(None)
 
-form_values['email'] = my_email
-form_values['password'] = sys.argv[1]
+form_values['email'] = sys.argv[1]
+form_values['password'] = sys.argv[2]
